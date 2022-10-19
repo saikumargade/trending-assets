e4160484baa75cbc00ad647ae9149d98354a595c
saikumargade <gade.saikumar1@gmail.com>
2022-10-20 01:16:54 +0530

    feat: added heading in the UI

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,13 @@ import { assetsData } from "./data";
 
 function App() {
   return (
-    <div className="group relative asset-container bg-blue-dark">
-      {assetsData.map((assetDetails) => (
-        <AssetCard assetDetails={assetDetails} />
-      ))}
+    <div className="body bg-blue-dark">
+      <p className="amount pt-48 pl-14"> Trending Assets</p>
+      <div className="group relative asset-container">
+        {assetsData.map((assetDetails) => (
+          <AssetCard assetDetails={assetDetails} />
+        ))}
+      </div>
     </div>
   );
 }
