e7e2a70f3285e4509e5f6c182b465becbcaff130
saikumargade <gade.saikumar1@gmail.com>
2022-10-19 21:45:59 +0530

    feat: render all asset cards

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,31 @@ function App() {
     <div className="group relative asset-container bg-blue-dark">
       <div className="h-80 bg-blue-light rounded-xl asset-card">
         <div className="asset-icon">
+          <img alt="bitcoin" src={require("./assets/bitcoin.png")} />
+        </div>
+        <div className="asset-content"></div>
+      </div>
+      <div className="h-80 bg-blue-light rounded-xl asset-card">
+        <div className="asset-icon">
           <img alt="solana" src={require("./assets/solana.png")} />
+        </div>
+        <div className="asset-content"></div>
+      </div>
+      <div className="h-80 bg-blue-light rounded-xl asset-card">
+        <div className="asset-icon">
+          <img alt="ethereum" src={require("./assets/ethereum.png")} />
+        </div>
+        <div className="asset-content"></div>
+      </div>
+      <div className="h-80 bg-blue-light rounded-xl asset-card">
+        <div className="asset-icon">
+          <img alt="binance-usd" src={require("./assets/binance-usd.png")} />
+        </div>
+        <div className="asset-content"></div>
+      </div>
+      <div className="h-80 bg-blue-light rounded-xl asset-card">
+        <div className="asset-icon">
+          <img alt="shiba-inu" src={require("./assets/shiba-inu.png")} />
         </div>
         <div className="asset-content"></div>
       </div>
