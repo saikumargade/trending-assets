d543c493d3172b710f85f4f9ddefc50694cfd556
saikumargade <gade.saikumar1@gmail.com>
2022-10-20 00:40:41 +0530

    refactor: app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,14 @@
 import React from "react";
 import "./App.css";
+import AssetCard from "./components/AssetCard";
+import { assetsData } from "./data";
 
 function App() {
   return (
     <div className="group relative asset-container bg-blue-dark">
-      <div className="h-80 bg-blue-light rounded-xl asset-card">
-        <div className="asset-icon">
-          <img alt="bitcoin" src={require("./assets/bitcoin.png")} />
-        </div>
-        <div className="asset-content"></div>
-      </div>
-      <div className="h-80 bg-blue-light rounded-xl asset-card">
-        <div className="asset-icon">
-          <img alt="solana" src={require("./assets/solana.png")} />
-        </div>
-        <div className="asset-content"></div>
-      </div>
-      <div className="h-80 bg-blue-light rounded-xl asset-card">
-        <div className="asset-icon">
-          <img alt="ethereum" src={require("./assets/ethereum.png")} />
-        </div>
-        <div className="asset-content"></div>
-      </div>
-      <div className="h-80 bg-blue-light rounded-xl asset-card">
-        <div className="asset-icon">
-          <img alt="binance-usd" src={require("./assets/binance-usd.png")} />
-        </div>
-        <div className="asset-content"></div>
-      </div>
-      <div className="h-80 bg-blue-light rounded-xl asset-card">
-        <div className="asset-icon">
-          <img alt="shiba-inu" src={require("./assets/shiba-inu.png")} />
-        </div>
-        <div className="asset-content"></div>
-      </div>
+      {assetsData.map((assetDetails) => (
+        <AssetCard assetDetails={assetDetails} />
+      ))}
     </div>
   );
 }
